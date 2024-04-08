de872ccb7c5981563a2140428fd59cb65ad50c8b
Daniel Janík <01djanik@gmail.com>
2024-04-08 14:06:38 +0200

    trail + player movement

--- src/js/Constants.js
+++ src/js/Constants.js
@@ -1,8 +1,13 @@
 // input variables
+export const KEY_W = 87;
 export const KEY_A = 65;
+export const KEY_S = 83;
 export const KEY_D = 68;
+// arrow keys
+export const UP_ARROW = 38;
+export const DOWN_ARROW = 40;
 export const LEFT_ARROW = 37;
 export const RIGHT_ARROW = 39;
 
 // player variables
-export const PLAYER_SPEED = 0.5;
+export const PLAYER_SPEED = 7;
--- src/js/game.js
+++ src/js/game.js
@@ -3,10 +3,16 @@ console.log('DEBUG TEST');
 import * as constants from './Constants.js';
 
 let sketch = (level) => {
-    let numColumns = 15;
-    let numRows = 15;
+    const numColumns = 10;
+    const numRows = 10;
+    const margin = 20;
+
     let grid = [];
-    let margin = 20;
+    let gridSize = 0;
+    let xOffset = 0;
+    let yOffset = 0;
+
+    let player;
 
     level.preload = () => {};
 
@@ -14,11 +20,15 @@ let sketch = (level) => {
         let canvas = level.createCanvas(level.windowWidth, level.windowHeight);
         canvas.parent('canvas-container');
         level.windowResized();
+        level.createGrid();
+        level.createPlayer();
     };
 
     level.draw = () => {
         level.background(50, 50, 50, 255);
+        level.playerMovement();
         level.drawGrid();
+        level.drawPlayer();
     };
 
     level.windowResized = () => {
@@ -27,14 +37,10 @@ let sketch = (level) => {
         level.resizeCanvas(containerWidth, level.windowHeight - footerHeight);
     };
 
-    level.drawGrid = () => {
-        let gridSize = Math.min(level.width - margin * 2, level.height - margin * 2) / Math.max(numColumns, numRows);
-        let xOffset = margin + (level.width - gridSize * numColumns - margin * 2) / 2;
-        let yOffset = margin + (level.height - gridSize * numRows - margin * 2) / 2;
-
-        level.stroke(255);
-        level.strokeWeight(1);
-
+    level.createGrid = () => {
+        gridSize = Math.min(level.width - margin * 2, level.height - margin * 2) / Math.max(numColumns, numRows);
+        xOffset = margin + (level.width - gridSize * numColumns - margin * 2) / 2;
+        yOffset = margin + (level.height - gridSize * numRows - margin * 2) / 2;
         for (let i = 0; i < numColumns; i++) {
             grid[i] = [];
             for (let j = 0; j < numRows; j++) {
@@ -45,24 +51,114 @@ let sketch = (level) => {
                     x: i,
                     y: j,
                     owner: 'none',
+                    fresh: false,
                     posX: x,
                     posY: y,
                     size: gridSize,
                 };
                 grid[i][j] = square;
-                level.noFill();
-                level.rect(x, y, gridSize, gridSize);
-                level.textAlign(level.CENTER, level.CENTER);
-                level.text(square.x + ',' + square.y, x + gridSize / 2, y + gridSize / 2);
             }
         }
     };
 
-    // TODO:
-    level.createPlayer = () => {};
+    level.drawGrid = () => {
+        level.stroke(255);
+        level.strokeWeight(1);
 
-    level.playerMovement = () => {};
+        for (let i = 0; i < numColumns; i++) {
+            for (let j = 0; j < numRows; j++) {
+                let square = grid[i][j];
+                level.noFill();
+                level.rect(square.posX, square.posY, gridSize, gridSize);
+                level.textAlign(level.CENTER, level.CENTER);
+                level.text(square.x + ',' + square.y, square.posX + gridSize / 2, square.posY + gridSize / 2);
 
-    console.log(grid);
+                if (square.fresh) {
+                    level.fill(255, 0, 0, 127);
+                    level.rect(square.posX, square.posY, gridSize, gridSize);
+                }
+            }
+        }
+    };
+
+    level.createPlayer = () => {
+        let randomX, randomY, randomFacing;
+        do {
+            randomX = Math.floor(Math.random() * numColumns);
+            randomY = Math.floor(Math.random() * numRows);
+            randomFacing = ['N', 'S', 'W', 'E'][Math.floor(Math.random() * 4)];
+        } while (grid[randomX][randomY].owner !== 'none');
+
+        player = {
+            x: randomX,
+            y: randomY,
+            size: gridSize,
+            facing: randomFacing,
+        };
+
+        grid[randomX][randomY].owner = 'player';
+    };
+
+    level.drawPlayer = () => {
+        level.fill(255, 0, 0);
+        level.rect(grid[player.x][player.y].posX, grid[player.x][player.y].posY, player.size, gridSize);
+    };
+
+    level.playerDirection = () => {
+        let nextSquare;
+
+        level.keyPressed = () => {
+            if (level.keyCode === constants.KEY_A || level.keyCode === level.LEFT_ARROW) {
+                if (player.facing !== 'E') {
+                    player.facing = 'W';
+                }
+            } else if (level.keyCode === constants.KEY_D || level.keyCode === level.RIGHT_ARROW) {
+                if (player.facing !== 'W') {
+                    player.facing = 'E';
+                }
+            } else if (level.keyCode === constants.KEY_W || level.keyCode === constants.UP_ARROW) {
+                if (player.facing !== 'S') {
+                    player.facing = 'N';
+                }
+            } else if (level.keyCode === constants.KEY_S || level.keyCode === constants.DOWN_ARROW) {
+                if (player.facing !== 'N') {
+                    player.facing = 'S';
+                }
+            }
+        };
+        // Determine the coordinates of the next square based on the player's facing direction
+        switch (player.facing) {
+            case 'N':
+                nextSquare = grid[player.x][player.y - 1];
+                break;
+            case 'S':
+                nextSquare = grid[player.x][player.y + 1];
+                break;
+            case 'W':
+                nextSquare = grid[player.x - 1] ? grid[player.x - 1][player.y] : null;
+                break;
+            case 'E':
+                nextSquare = grid[player.x + 1] ? grid[player.x + 1][player.y] : null;
+                break;
+        }
+        return nextSquare;
+    };
+
+    level.playerMovement = () => {
+        let currentSquare = grid[player.x][player.y];
+        let nextSquare = level.playerDirection();
+        // Move the player to the next square if it's not occupied and not null
+        if (nextSquare) {
+            currentSquare.fresh = true;
+            nextSquare.owner = 'player';
+            player.x = nextSquare.x;
+            player.y = nextSquare.y;
+        }
+        let speed = constants.PLAYER_SPEED;
+        level.frameRate(speed);
+    };
+
+    level.fillSpace = () => {};
 };
+
 let p5level = new p5(sketch);
